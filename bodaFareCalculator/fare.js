8a7bdced56fe3fba5e837f5d2465402937a000c1
Agostino <agostinoscholes572@gmail.com>
2025-06-08 00:17:43 +0300

    made changes

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,18 +1,18 @@
-function calculateBodaFare(distanceInKm){
+function calculateBodaFare(){
+let distanceInKm = prompt("Unafika wapi Mkubwa? Kilometer ngapi?:");
+    if(isNaN(distanceInKm) || distanceInKm <=0) {
+        alert("Tafadhali ingiza nambari sahihi ya kilomita.");
+    return;}
     const baseFare = 50;
     const chargePerKm = 15;
     const tripFare = distanceInKm * chargePerKm;
     const totalFare = baseFare + tripFare;
+
     console.log(`Uko kwote? Io ni ${distanceInKm} km:`);
     console.log(`Ukikalia Pikipiki: KES ${baseFare}`);
     console.log(`Mpaka Uko: KES ${tripFare}`);
-    console.log(`Total: KES ${totalFare}\n`);
-    console.log("Panda Pikipiki!");
+    console.log(`Total: KES ${totalFare}`);
+    console.log("\n Panda Pikipiki!"); 
 }
- const input = prompt("Unafika wapi Mkubwa? Kilometer ngapi?");
- const distance = Number(input);     
- if (isNaN(distance) || distance <=0) {
-    console.log("Tafadhali ingiza nambari sahihi ya kilomita.");
- } else {
-    calculateBodaFare(distance);
- }
+ calculateBodaFare();
+    
